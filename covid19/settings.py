9bad2d82ff668e59cc5b7ff049ed0f15006e26b5
FredrickSimi <fkemosi@gmail.com>
2021-08-11 20:05:08 +0300

    11082021 4thchange

--- covid19/settings.py
+++ covid19/settings.py
@@ -143,8 +143,8 @@ USE_TZ = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'fkemosi@gmail.com'
-EMAIL_HOST_PASSWORD = 'Yzzwyxfczxjbpvyym'
+EMAIL_HOST_USER = 'you@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL=False
